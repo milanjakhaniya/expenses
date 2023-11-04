b16145d579786509453222813dcbb9831a2f5ddf
Milan Jakhaniya <milanpatel1082@gmail.com>
2023-11-04 18:24:23 +0530

    almost complete with sheet one

--- expenses_app/models.py
+++ expenses_app/models.py
@@ -3,17 +3,23 @@ from django.db import models
 class ExcelData(models.Model):
     date = models.DateField()
     time = models.TimeField()
-    shop_code = models.IntegerField()
-    product_id = models.IntegerField()
-    product_type = models.IntegerField()
-    weight = models.DecimalField(max_digits=25, decimal_places=2)
-    quantity = models.DecimalField(max_digits=25, decimal_places=2)
-    daily_rate = models.DecimalField(max_digits=25, decimal_places=2)
-    rate = models.DecimalField(max_digits=25, decimal_places=2)
+    category = models.TextField(max_length=255)
+    category_code = models.IntegerField()
+    description = models.TextField(max_length=255)
+    payment_mode = models.TextField(max_length=50)
     amount = models.DecimalField(max_digits=25, decimal_places=2)
-    opening_balance = models.DecimalField(max_digits=25, decimal_places=2)
-    paid_amount = models.DecimalField(max_digits=25, decimal_places=2)
-    closing_balance = models.DecimalField(max_digits=25, decimal_places=2)
+    complaint = models.TextField(max_length=255)
+
+    # product_id = models.IntegerField()
+    # product_type = models.IntegerField()
+    # weight = models.DecimalField(max_digits=25, decimal_places=2)
+    # quantity = models.DecimalField(max_digits=25, decimal_places=2)
+    # daily_rate = models.DecimalField(max_digits=25, decimal_places=2)
+    # rate = models.DecimalField(max_digits=25, decimal_places=2)
+    
+    # opening_balance = models.DecimalField(max_digits=25, decimal_places=2)
+    # paid_amount = models.DecimalField(max_digits=25, decimal_places=2)
+    # closing_balance = models.DecimalField(max_digits=25, decimal_places=2)
 
     def __str__(self):
         return f"Entry {self.pk}: {self.date}"
--- expenses_app/urls.py
+++ expenses_app/urls.py
@@ -1,23 +1,8 @@
-"""expenses URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create_excel/<str:sheet_name>/', views.excel_view, name='create_excel'),
-    path('create_daily_summary_sheet/<str:sheet_name>/',views.create_daily_summary_sheet, name='create_daily_summary_sheet'),
+    path('create_excel/<str:sheet_name>/', views.create_excel, name='create_excel'),
+    # path('create_daily_summary_sheet/<str:sheet_name>/',views.create_daily_summary_sheet, name='create_daily_summary_sheet'),
 
 ]
